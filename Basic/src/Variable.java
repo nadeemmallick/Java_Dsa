21db419975e37973da5f6950077cca11aa417370
nadeemmallick <nadeemmallick069@gmail.com>
2026-04-21 12:06:01 +0530

    day1

---
+++ Basic/src/Variable.java
@@ -0,0 +1,42 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        //declaring variable
+        int ages ;
+        ages = 10; // assigning value to variable
+        System.out.println(ages);
+
+        //define or initialize
+        int muNumber = 10;
+        System.out.println(muNumber);
+
+        //case-sensitive
+        int weight = 10;
+        int WEIGHT = 100;
+        System.out.println(weight);
+        System.out.println(WEIGHT);
+
+        //Starting Letter
+        int TOTAL = 23;
+        int total = 23;
+        int _total = 23;
+        int $total = 23;
+
+        //subsequent letter
+        int height_my = 2;
+        int height3 = 3;
+        int height$12my = 6;
+        int height$ = 7;
+
+        //no reversed keyword not usable
+        //int class = 5;
+
+
+
+
+
+
+
+
+    }
+}
