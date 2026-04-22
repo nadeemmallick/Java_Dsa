4b904008871df9d6d8dd36416872e993a5c66006
nadeemmallick <nadeemmallick069@gmail.com>
2026-04-22 11:18:18 +0530

    Revised all data typen in java and review all examples of data types.(day2)

---
+++ Basic/src/dataType.java
@@ -0,0 +1,34 @@
+public class dataType {
+    static void main() {
+        //numeric data type
+        byte b = 127;
+        System.out.println(b);
+
+        short s = 32767;
+        System.out.println(s);
+
+        int i = 2147483647;
+        System.out.println(i);
+
+        long l = 9223372036854775807L;
+        System.out.println(l);
+
+        //Floating point data type
+        float f = 3.14f;
+        System.out.println(f);
+
+        double d = 3.14666666;
+        System.out.println(d);
+
+        //boolean data type
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b1);
+        System.out.println(b2);
+
+        char c = 'A';
+        System.out.println(c);
+
+
+    }
+}
