47e1515a29969400326f102237bb8460cb9c6845
nadeemmallick <nadeemmallick069@gmail.com>
2026-04-26 09:21:19 +0530

    day6

---
+++ Patterns/src/SolidRectangular.java
@@ -0,0 +1,11 @@
+public class SolidRectangular {
+    static void main() {
+        int n = 3;
+        for(int i=1; i<=3; i++){
+            for(int j =1; j<=n+2; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
